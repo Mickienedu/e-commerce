68e79ecda419dbb3502bafa26a001f0675fd938d
Michael Okorie <mickienedu@gmail.com>
2024-08-10 23:03:18 +0100

    added media querry in 500px

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -2,8 +2,9 @@
 import women_1 from './women1.jpeg';
 import women_2 from './women2.jpeg';
 import women_3 from './women3.jpeg';
-import women_4 from './women4.jpeg';
 import women_5 from './women5.jpeg';
+import women_001 from './20240711_224723.png';
+import women_00 from './20240711_230451.png';
 
 
 let data_product = [
@@ -29,16 +30,24 @@ let data_product = [
     old_price:160.5,
     },
     {
+    id: 5,
+        name: "Striped flutter Sleeve overlap collar pepium hem blouse",
+        image: women_001,
+        new_price: 210.0,
+        old_price:300.5,
+        },
+    {
     id: 4,
     name: "Striped flutter Sleeve overlap collar pepium hem blouse",
-    image: women_4,
+    image: women_5,
     new_price: 210.0,
     old_price:300.5,
     },
+   
     {
-    id: 5,
+    id: 6,
     name: "Striped flutter Sleeve overlap collar pepium hem blouse",
-    image: women_5,
+    image: women_00,
     new_price: 210.0,
     old_price:300.5,
     },
